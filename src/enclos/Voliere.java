f0096eb0ca4455d7db52b9c20487f95ccacdbe49
pesudo_github <fangjingyao8@gmail.com>
2022-05-02 15:02:44 +0200

    Changes by PLucas

--- src/enclos/Voliere.java
+++ src/enclos/Voliere.java
@@ -1,6 +1,10 @@
 package enclos;
 
 public class Voliere extends Enclos{
+
+    private double hauteur;
+    private boolean veriftoit;
+
     public Voliere(String nom, double superficie, int nb_max_animaux, int nb_animaux_pres, String proprete) {
         super(nom, superficie, nb_max_animaux, nb_animaux_pres, proprete);
     }
